a1de8e9cbb599cf731933ae74a71118792aca134
cyngko <christianboenigk@gmx.de>
2021-06-12 09:26:14 +0200

    added animation on projects page

--- app.js
+++ app.js
@@ -51,33 +51,24 @@ function enableScroll() {
 
 disableScroll();
 
-setTimeout(
-  (window.onload = function () {
+// setTimeout(
+//   (window.onload = function () {
+//     document.querySelector('.preloader').style.opacity = '0';
+//     enableScroll();
+//   }),
+//   2000
+// );
+
+let stateCheck = setInterval(() => {
+  if (document.readyState === 'complete') {
     document.querySelector('.preloader').style.opacity = '0';
     enableScroll();
-  }),
-  2000
-);
+    clearInterval(stateCheck);
+    // document ready
+  }
+}, 100);
 
-// let stateCheck = setInterval(() => {
-//   if (document.readyState === 'complete') {
-//     document.querySelector('.preloader').style.opacity = '0';
-//     enableScroll();
-//     clearInterval(stateCheck);
-//     // document ready
-//   }
-// }, 100);
 
-// let stateCheck = setInterval(isLoaded, 100)
-
-// function isLoaded() {
-//   if (document.readyState === 'complete') {
-//     document.querySelector('.preloader').style.opacity = '0';
-//     enableScroll();
-//     clearInterval(stateCheck);
-// }
-
-// setTimeout(stateCheck, 2000);
 
 // Hide header while scrolling
 var prevScrollpos = window.pageYOffset;
